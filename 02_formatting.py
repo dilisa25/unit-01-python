d7b28672bd95ce5beeb641ca6a9661a64a8025be
dilisa25 <dward@brooklynsteamcenter.org>
2025-09-12 10:50:31 -0400

    might be cooked

---
+++ 02_formatting.py
@@ -0,0 +1,85 @@
+"""
+TASK 1:
+Write code that checks if a user entered the correct password.
+The password should not be case sensitive
+
+"""
+print("")
+print("----Task1-----")
+print("")
+correct_password = "cat"
+
+password = input("Type password: ")
+
+if password.lower() == correct_password:
+    print("correct")
+
+
+
+
+print("")
+print("----Task1-----")
+print("")
+"""
+TASK 2:
+Write code that checks if a user inputs an empty string
+If the string is empty, print "invalid" otherwise print "valid"
+"""
+print("")
+print("----Task2-----")
+print("")
+
+task = input("type something : ")
+
+if task == "" 
+  
+  
+
+
+print("")
+print("----Task2-----")
+print("")
+"""
+TASK 3:
+
+Write a program that will replace the word "cat" with the word "dog"
+It should replace all occurances regardless of captilization 
+"""
+print("")
+print("----Task3-----")
+print("")
+
+sentence = "I have a cat and a dog"
+new_sentence = sentence.replace ("cat","dog")
+print(new_sentence)
+
+print("")
+print("----Task3-----")
+print("")
+
+"""
+TASK 4:
+
+Write a program that takes a person's name and age as input and prints it
+"""
+print("")
+print("----Task4-----")
+print("")
+
+print("")
+print("----Task4-----")
+print("")
+
+"""
+TASK 5:
+
+Write a program that takes in two floats, and prints the quotient
+The result should be rounded to the nearest tenth (1 decimal place)
+"""
+print("")
+print("----Task5-----")
+print("")
+
+print("")
+print("----Task5-----")
+print("")
