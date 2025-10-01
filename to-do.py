a44d11f13576a24f15031f5e3178858f61596536
dilisa25 <dward@brooklynsteamcenter.org>
2025-10-01 10:52:42 -0400

    open

--- to-do.py
+++ to-do.py
@@ -1,6 +1,10 @@
-todolist=[]
-#create while loop so code runs forever 
+with open("todo.txt") as file: 
 
+    todolist = file.readlines()
+
+
+
+#create while loop so code runs forever 
 while True:
     print("Your To-Do List:")
     print()
@@ -10,10 +14,10 @@ while True:
  
     
     #ask user if they would like to add or delete  
-    add_del= input("Would you like to add or delete?: ").strip().lower()
+    choice= input("Would you like to 'add' or 'delete' or 'exit' the program ?: ").strip().lower()
     
     #if they pick add, add item to list using append 
-    if add_del == "add":
+    if choice == "add":
         print()
         add = input("What would you like to add?: ").strip().lower()
     
@@ -23,10 +27,16 @@ while True:
         print()
      
     #if they pick delete, delete item to list using del  
-    if add_del == "delete":
+    if choice == "delete":
         print()
         delete = int(input("What number task would like to remove?: ").strip().lower())
         print()
-        #User doesnt know computer starts counting at 1 so [delete - 1]
+        #User doesnt know computer starts counting at 0 so [delete - 1]
         del todolist [delete - 1]
-      
+    if choice == "exit":
+        with open("todo.txt") as file: 
+
+         todolist = file.writelines()
+         
+        break
+    
