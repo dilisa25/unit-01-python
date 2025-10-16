e33e87e5e7c5adfb1b12c93b41f63ba4e4f5102b
dilisa25 <dward@brooklynsteamcenter.org>
2025-10-15 20:48:36 -0400

    commit

--- 10_dubious_dates.py
+++ 10_dubious_dates.py
@@ -30,14 +30,16 @@ Exercise 3:
 Using the strptime function, convert two strings into dates.
 Then find the difference in days between the two.
 """
+#Two random dates as strings  
 string1 = "09/11/2008"
 string2 = "June 22,2025"
-
+#Used the strptime function to convert the strings into dates
 date1 = datetime.strptime(string1, "%m/%d/%Y")
 date2 = datetime.strptime(string2, "%B %d,%Y")
 
 print(date1)
 print(date2)
+#The found the difffrence between the dates
 print(date2 - date1)
 
 """
@@ -45,9 +47,11 @@ Excercise 4:
 Write a program that asks the user for their birthdate and calculates their current 
 age using the datetime module.
 """
+# Asks the user for their birthdate then convert the string into a date
 your_birthday = input("Whats your birthday?(mm/dd/yyyy): ")
 birthday = datetime.strptime(your_birthday, "%m/%d/%Y")
 today = datetime.now()
+#To get age subract todays year from birthday year 
 age = today.year - birthday.year
 print("Your age is ",age) 
 
