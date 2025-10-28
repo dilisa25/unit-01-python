1d2197ff5b342408f945fd62f2c829ac36592db4
dilisa25 <dward@brooklynsteamcenter.org>
2025-10-28 09:59:56 -0400

    unit_testing

---
+++ 15_trippy_testing.py
@@ -0,0 +1,103 @@
+"""
+Exercise 1: Divide
+"""
+def divide(a, b):
+  """Divides two numbers, handling potential division by zero.
+
+  Args:
+    a: The numerator.
+    b: The denominator.
+
+  Returns:
+    The quotient, or None if b is zero.
+  """
+
+  if b == 0:
+    return None
+  else:
+    return a / b
+#Ensures that the divsion is correct. 16/2 = 8
+assert divide(16, 2) == 8
+
+
+
+"""
+Exercise 2: Factorial
+"""
+def factorial(n):
+  """Calculates the factorial of a non-negative integer.
+
+  Args:
+    n: A non-negative integer.
+
+  Returns:
+    The factorial of n.
+  """
+
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+#Ensures that the factorial equation is correct. 
+assert factorial(4) == 24
+
+"""
+Exercise 3: String Reverse
+"""
+def reverse_string(string):
+  """Reverses a given string.
+
+  Args:
+    string: A string to be reversed.
+
+  Returns:
+    The reversed string.
+  """
+
+  reversed_string = ""
+  for char in string:
+    reversed_string = char + reversed_string
+  return reversed_string
+#Ensures that the word is reversed correctly.
+assert reverse_string("cat") == "tac"
+"""
+Exercise 4: Fibonacci
+"""
+def fibonacci(n):
+  """Generates the nth Fibonacci number.
+
+  Args:
+    n: The index of the Fibonacci number to calculate.
+
+  Returns:
+    The nth Fibonacci number.
+  """
+
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+#Ensures that the word is fibonacci ewaution is correct.
+assert fibonacci(4) == 3
+"""
+Exercise 5: Email Validation
+"""
+
+import re
+
+def is_valid_email(email):
+  """Determines whether email is valid or not
+
+  Args:
+    email: The email to validate
+
+  Returns:
+    Boolean value if email is valid
+  """
+  email_regex = r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+"
+  return re.match(email_regex, email) is not None
+#Ensures that email is a valid email. 
+assert is_valid_email("dward@brooklynsteamcenter.org") == True
